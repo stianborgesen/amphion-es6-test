b0dd140655616a6584e5b9c1f7d3a973853200c1
stianborgesen <stianfb@stud.ntnu.no>
2020-04-07 16:02:23 +0200

    2 files modified

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ import './style.css';
 import { Ros } from 'roslib'
 import Amphion from 'amphion'
 
+const ur_description = "https://raw.githubusercontent.com/fmauch/universal_robot/kinetic-devel/ur_description"
+const franka_description = "https://raw.githubusercontent.com/frankaemika/franka_ros/kinetic-devel/franka_description"
+
+const ur_packages = {robot_description: ur_description}
+
+const franka_packages = {franka_description: franka_description}
+
 function loadRobot(viewer){
-  const robotModel = new Amphion.RobotModel(
-    ros, 'robot_description',
-    {
-      packages:{
-        robot_description:"https://raw.githubusercontent.com/fmauch/universal_robot/kinetic-devel/ur_description"
-      }
-    }
-  )
+  const robotModel = new Amphion.RobotModel(ros, 'robot_description',{packages:franka_packages})
   robotModel.load()
   viewer.addVisualization(robotModel)
 }
 
 function loadTf(viewer){
-  const path = new Amphion.Path(ros, '/path_rosbag');
+  const path = new Amphion.Path(ros, '/panda_tf');
   path.subscribe();
   path.updateOptions({
     color: 0xff0000,
@@ -30,15 +30,11 @@ const url = 'ws://localhost:9090'
 const ros = new Ros();
 
 //const scene = new Amphion.Scene();
-const viewer = new Amphion.Viewer3d();
+//const viewer = new Amphion.Viewer3d();
 const viewer = new Amphion.TfViewer(ros);
 
 //const viewer = new Amphion.TfViewer(ros);
 viewer.setContainer(document.getElementById('scene3d'));
 ros.connect(url)
-
-function load(){
-
-}
-
-loadRobot(viewer)
+loadTf(viewer)
+//loadRobot(viewer)
