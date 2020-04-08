2817d863cb1206569092f5491c396df36165163b
stianborgesen <stianfb@stud.ntnu.no>
2020-04-08 12:04:57 +0200

    2 files modified

--- index.js
+++ index.js
@@ -9,32 +9,40 @@ const franka_description = "https://raw.githubusercontent.com/frankaemika/franka
 const ur_packages = {robot_description: ur_description}
 
 const franka_packages = {franka_description: franka_description}
+class Loader{
+  constructor(url){
+    this.url = url
+    this.ros = new Ros()
+    this.viewer = new Amphion.Viewer3d()
+    this.viewer.setContainer(document.getElementById('scene3d'));
+    this.ros.connect(this.url)
+  }
 
-function loadRobot(viewer){
-  const robotModel = new Amphion.RobotModel(ros, 'franka_description',{packages:franka_packages})
-  robotModel.load()
-  viewer.addVisualization(robotModel)
+  loadRobot(packages){
+    const robotModel = new Amphion.RobotModel(this.ros, 'robot_description',{packages:packages})
+    robotModel.load()
+    this.viewer.addVisualization(robotModel)
+  }
+
+  loadTf(path){
+    const path = new Amphion.Path(this.ros, path);
+    path.subscribe();
+    path.updateOptions({
+      color: 0xff0000,
+    });
+    this.viewer.addVisualization(path);
+  }
 }
-
-function loadTf(ros, viewer){
-  const path = new Amphion.Path(ros, '/pose_array_rosbag');
-  path.subscribe();
-  path.updateOptions({
-    color: 0xff0000,
-  });
-  viewer.addVisualization(path);
-}
-
 const url = 'ws://localhost:9090'
-
-const ros = new Ros();
+const loader = new Loader(url)
+loader.loadTf("/path_rosbag")
+loader.loadRobot(franka_packages)
+//const ros = new Ros();
 
 //const scene = new Amphion.Scene();
 //const viewer = new Amphion.Viewer3d();
-const viewer = new Amphion.TfViewer(ros);
-
 //const viewer = new Amphion.TfViewer(ros);
-viewer.setContainer(document.getElementById('scene3d'));
-ros.connect(url)
-loadTf(ros, viewer)
-loadRobot(viewer)
+//viewer.setContainer(document.getElementById('scene3d'));
+//ros.connect(url)
+//loadTf(ros, viewer, '/path_rosbag')
+//loadRobot(viewer, franka_packages)
